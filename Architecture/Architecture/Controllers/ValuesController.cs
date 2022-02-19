07dba112e2af6262b00eea72ef19d0bbbb63dffb
Muhammad Sohaib Nasir <sohaibnasir40@gmail.com>
2022-02-19 20:30:25 +0500

    Commit

--- Architecture/Architecture/Controllers/ValuesController.cs
+++ Architecture/Architecture/Controllers/ValuesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -18,6 +19,7 @@ namespace Architecture.Controllers
 
         // GET api/<ValuesController>/5
         [HttpGet("{id}")]
+        [Authorize]
         public string Get(int id)
         {
             return "value";
