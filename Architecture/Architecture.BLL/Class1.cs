ff0d072c1b58c6181463e27281c55c833a423679
Muhammad Sohaib Nasir <sohaibnasir40@gmail.com>
2022-02-19 18:50:26 +0500

    Initial Commit

---
+++ Architecture/Architecture.BLL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Architecture.BLL
+{
+    public class Class1
+    {
+    }
+}
---
+++ Architecture/Architecture.DAL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Architecture.DAL
+{
+    public class Class1
+    {
+    }
+}
